5491c7250a05c79b44b457e1d4af52787256dc62
MikeStone20 <michael.stone0320@gmail.com>
2021-07-19 21:38:42 -0600

    Hello world

---
+++ HelloWorld/hello_world.rs
@@ -0,0 +1,12 @@
+fn main(){
+    
+    println!("Hello, World!");
+
+}
+
+/*
+ * fn main is how we declare main function
+ * Main function is basically the same as the main in c/c++
+ * println! --> This is a macro, functions dont have !
+ * rustc *name of file* to compile
+ */
